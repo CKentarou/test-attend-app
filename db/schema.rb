c8b418dac91d18f20b98a38f9484e6d04c95f638
かれん <k023c0029@g.neec.ac.jp>
2025-12-18 11:35:34 +0900

    科目作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_18_011626) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_18_022104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.integer "teacher_id"
+    t.string "classroom"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_subjects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +37,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_18_011626) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "subjects", "users"
 end
